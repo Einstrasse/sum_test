96e50f0a12188952a47703bba90811334225d03a
Einstrasse <hg9587@naver.com>
2017-07-05 10:08:31 +0000

    refactoring for function

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # sum_test
-bob6th consulting 정한길 프로그래밍 과제
+bob 6기 보안 컨설팅 트랙 정한길    
+1부터 100까지의 합을 출력하는 과제
 
 ### Python3 버전
 
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-summation=0
-for i in range(1, 101):
-	summation+=i
-print (summation)
+def sum(n):
+	summation=0;
+	for i in range(1, n+1):
+		summation+=i
+	return summation
+
+track="""컨설팅"""
+name="""정한길"""
+
+print ('[bob6][%s]sum_test[%s]' % (track, name))
+print (sum(100))
